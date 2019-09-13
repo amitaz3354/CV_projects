a22bebd817bf77c6c61d7b78fc587e0f9d8f2c65
amitaz3354 <51985287+amitaz3354@users.noreply.github.com>
2019-09-14 00:03:23 +0300

    Add files via upload

---
+++ Facedetection.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import time
+
+
+img = cv2.imread('amit2.jpg')
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+faces = face_cascade.detectMultiScale(gray,1.1,4)
+
+print('Faces found: ', len(faces))
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+      
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
